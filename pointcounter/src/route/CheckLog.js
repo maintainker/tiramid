e2bfb348a5dcb332994d7518d842f17358338769
maintainker <syhwan88@naver.com>
2020-12-03 11:06:10 +0900

    [fix] font 수정, 승리에 티라미드시 승률반영안됨

--- pointcounter/src/route/CheckLog.js
+++ pointcounter/src/route/CheckLog.js
@@ -1,48 +1,60 @@
 import Nav from "../components/Nav";
 import styled from "styled-components";
 const StyledLogUl = styled.ul`
-  margin:30px auto;
-  list-style:none;
-  max-height:450px;
-  overflow:scroll;
-  li{
-    div{
-      display:inline-block;
+  margin: 30px auto;
+  list-style: none;
+  max-height: 450px;
+  overflow: scroll;
+  padding: 0;
+  li {
+    div {
+      display: inline-block;
     }
   }
-
 `;
 const StyledTimeDiv = styled.div`
-  width:70px;
-  height:28px;
-  line-height:28px;
+  width: 70px;
+  height: 28px;
+  line-height: 28px;
+  font-size: 12px;
 `;
 const StyledPlayerDiv = styled.div`
-  width:60px;
+  width: 60px;
+  font-size: 12px;
 `;
-const CheckLog = ({logs}) =>{
-  return (  
-  <>
-  <Nav state="viewlog"/>
-  <StyledLogUl>
-    <li>
-      <StyledTimeDiv>시간</StyledTimeDiv>
-      <StyledPlayerDiv>승리1</StyledPlayerDiv>
-      <StyledPlayerDiv>승리2</StyledPlayerDiv>
-      <StyledPlayerDiv>패배1</StyledPlayerDiv>
-      <StyledPlayerDiv>패배2</StyledPlayerDiv>
-    </li>
-    {logs&& logs.map((log)=>(
-    <li key={log.timeStamp}>
-      <StyledTimeDiv>{log.timeStamp}</StyledTimeDiv>
-      <StyledPlayerDiv>{log.winner1}({log.winner1_point})</StyledPlayerDiv>
-      <StyledPlayerDiv>{log.winner2}({log.winner2_point})</StyledPlayerDiv>
-      <StyledPlayerDiv>{log.loser1}({log.loser1_point})</StyledPlayerDiv>
-      <StyledPlayerDiv>{log.loser2}({log.loser2_point})</StyledPlayerDiv>
-    </li>))}
-  </StyledLogUl>
-  </>);
-}
-
+const CheckLog = ({ logs }) => {
+  return (
+    <>
+      <Nav state="viewlog" />
+      <StyledLogUl>
+        <li>
+          <StyledTimeDiv>시간</StyledTimeDiv>
+          <StyledPlayerDiv>승리1</StyledPlayerDiv>
+          <StyledPlayerDiv>승리2</StyledPlayerDiv>
+          <StyledPlayerDiv>패배1</StyledPlayerDiv>
+          <StyledPlayerDiv>패배2</StyledPlayerDiv>
+        </li>
+        {logs &&
+          logs.map((log) => (
+            <li key={log.timeStamp}>
+              <StyledTimeDiv>{log.timeStamp}</StyledTimeDiv>
+              <StyledPlayerDiv>
+                {log.winner1}({log.winner1_point})
+              </StyledPlayerDiv>
+              <StyledPlayerDiv>
+                {log.winner2}({log.winner2_point})
+              </StyledPlayerDiv>
+              <StyledPlayerDiv>
+                {log.loser1}({log.loser1_point})
+              </StyledPlayerDiv>
+              <StyledPlayerDiv>
+                {log.loser2}({log.loser2_point})
+              </StyledPlayerDiv>
+            </li>
+          ))}
+      </StyledLogUl>
+    </>
+  );
+};
 
 export default CheckLog;
--- pointcounter/src/route/WinRatio.js
+++ pointcounter/src/route/WinRatio.js
@@ -1,80 +1,106 @@
 import Nav from "../components/Nav";
 
-const WinRatio =({logs}) =>{
-  const ratio = (()=>{
+const WinRatio = ({ logs }) => {
+  const ratio = (() => {
     let players = [];
     let tmpRatio = [];
-    for(let i in logs){
-      if(logs[i].loser1!== "티라미드" && players.indexOf(logs[i].winner1)===-1){
+    for (let i in logs) {
+      if (
+        logs[i].loser1 !== "티라미드" &&
+        players.indexOf(logs[i].winner1) === -1
+      ) {
         players.push(logs[i].winner1);
         tmpRatio.push({
-          name:logs[i].winner1,
-          win:1,
-          lose:0
-        })
-      }else if(logs[i].loser1!== "티라미드"){
+          name: logs[i].winner1,
+          win: 1,
+          lose: 0,
+        });
+      } else if (logs[i].loser1 !== "티라미드") {
         tmpRatio[players.indexOf(logs[i].winner1)].win++;
       }
-      if(logs[i].loser2!== "티라미드" && players.indexOf(logs[i].winner2)===-1){
+      if (
+        logs[i].loser2 !== "티라미드" &&
+        players.indexOf(logs[i].winner2) === -1
+      ) {
         players.push(logs[i].winner2);
         tmpRatio.push({
-          name:logs[i].winner2,
-          win:1,
-          lose:0
-        })
-      }else if(logs[i].loser2!== "티라미드"){
+          name: logs[i].winner2,
+          win: 1,
+          lose: 0,
+        });
+      } else if (logs[i].loser2 !== "티라미드") {
         tmpRatio[players.indexOf(logs[i].winner2)].win++;
       }
-      if(players.indexOf(logs[i].loser1)===-1){
+      if (players.indexOf(logs[i].loser1) === -1) {
         players.push(logs[i].loser1);
         tmpRatio.push({
-          name:logs[i].loser1,
-          win:0,
-          lose:1
-        })
-      }else{
+          name: logs[i].loser1,
+          win: 0,
+          lose: 1,
+        });
+      } else if (logs[i].winner1 !== "티라미드") {
         tmpRatio[players.indexOf(logs[i].loser1)].lose++;
       }
-      if(players.indexOf(logs[i].loser2)===-1){
+      if (players.indexOf(logs[i].loser2) === -1) {
         players.push(logs[i].loser2);
         tmpRatio.push({
-          name:logs[i].loser2,
-          win:0,
-          lose:1
-        })
-      }else{
+          name: logs[i].loser2,
+          win: 0,
+          lose: 1,
+        });
+      } else if (logs[i].winner2 !== "티라미드") {
         tmpRatio[players.indexOf(logs[i].loser2)].lose++;
       }
     }
     return tmpRatio;
   })();
-  const checkPlayers = ratio.filter((player)=>{
-    if(player.name==="티라미드" || player.win+player.lose <5){
+  const checkPlayers = ratio.filter((player) => {
+    if (player.name === "티라미드" || player.win + player.lose < 5) {
       return false;
-    }else{
+    } else {
       return true;
     }
-  })
-  checkPlayers.sort((a,b)=>{
-    const aRatio = a.win/(a.win+a.lose);
-    const bRatio = b.win/(b.win+b.lose);
-    return bRatio-aRatio
-  })
-  return(
-  <>
-    <Nav state="winRatio"/>
-    <div style={{margin:"20px 0 30px",position:"relative",maxHeight:"450px",overflow:"scroll"}}>
-    {checkPlayers.map((player,idx)=>
-    <div key={idx} style={{padding:"8px"}}>
-      <span>{idx+1}위 {player.name} 승률 : {Math.round((player.win/(player.win+player.lose))*10000)/100} % , win : {player.win}, lose : {player.lose}</span>
-    </div>)}
-    </div>
-    <span style={{
-    position: "absolute",
-    bottom: "3px",
-    right: "0",
-    fontSize: "5px"}}>* 합계 5판 이상만 집계됩니다. player에 이름이 있지만 여기에 이름이 없다면 경기수가 5판이 되지 않습니다.</span>
-  </>);
-}
+  });
+  checkPlayers.sort((a, b) => {
+    const aRatio = a.win / (a.win + a.lose);
+    const bRatio = b.win / (b.win + b.lose);
+    return bRatio - aRatio;
+  });
+  return (
+    <>
+      <Nav state="winRatio" />
+      <div
+        style={{
+          margin: "20px 0 30px",
+          position: "relative",
+          maxHeight: "450px",
+          overflow: "scroll",
+        }}
+      >
+        {checkPlayers.map((player, idx) => (
+          <div key={idx} style={{ padding: "8px" }}>
+            <span>
+              {idx + 1}위 {player.name} 승률 :{" "}
+              {Math.round((player.win / (player.win + player.lose)) * 10000) /
+                100}{" "}
+              % , win : {player.win}, lose : {player.lose}
+            </span>
+          </div>
+        ))}
+      </div>
+      <span
+        style={{
+          position: "absolute",
+          bottom: "3px",
+          right: "0",
+          fontSize: "5px",
+        }}
+      >
+        * 합계 5판 이상만 집계됩니다. player에 이름이 있지만 여기에 이름이
+        없다면 경기수가 5판이 되지 않습니다.
+      </span>
+    </>
+  );
+};
 
 export default WinRatio;
