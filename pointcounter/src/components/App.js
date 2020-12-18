b75ea3e4b65622e3342630a0370c37187457d7e5
maintainker <syhwan88@naver.com>
2020-12-18 12:22:28 +0900

    [feat] 두리사진 삭제

--- pointcounter/src/components/App.js
+++ pointcounter/src/components/App.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 import AppRoute from "./Router";
 import { useState, useEffect, useMemo } from "react";
-import {dbService} from "../fbase";  
+import { dbService } from "../fbase";
 const StyledContainer = styled.div`
-  display: ${(props)=>props.display==="true"? "block;":"none;"}
+  display: ${(props) => (props.display === "true" ? "block;" : "none;")}
   z-index:100;
   width: 500px;
   position: absolute;
@@ -21,43 +21,55 @@ const StyledContainer = styled.div`
 const StyledBtn = styled.button`
   border: 0;
   background: white;
-  position:absolute;
-  height:20px;
-  top:40px;
-  left:50%;
-  transform:translateX(-50%);
-  padding:20px;
-  line-height:0;
+  position: absolute;
+  height: 20px;
+  top: 40px;
+  left: 50%;
+  transform: translateX(-50%);
+  padding: 20px;
+  line-height: 0;
   @media (max-width: 768px) {
-    top:40px;
+    top: 40px;
   }
-  
 `;
 function App() {
-  const [logs,setLogs] = useState([]);
-  const year = (new Date()).getFullYear();
-  const month = (new Date()).getMonth()+1;
+  const [logs, setLogs] = useState([]);
+  const year = new Date().getFullYear();
+  const month = new Date().getMonth() + 1;
   // const month = 11;
-  useEffect(()=>{
-    dbService.collection(`playerList${year}${month}`).onSnapshot((snapshot)=>{
-    const logArr = snapshot.docs.map(doc=>({id:doc.id,...doc.data()}));
-    logArr.sort((a,b)=>Number(b.timeStamp) - Number(a.timeStamp));
-    setLogs(logArr);
-    })
-  },[])
-  const randomBg = useMemo(()=> Math.floor(Math.random()*15),[]);
-  const [display,setDisplay] =useState(true);
+  useEffect(() => {
+    dbService.collection(`playerList${year}${month}`).onSnapshot((snapshot) => {
+      const logArr = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      logArr.sort((a, b) => Number(b.timeStamp) - Number(a.timeStamp));
+      setLogs(logArr);
+    });
+  }, []);
+  const randomBg = useMemo(() => Math.floor(Math.random() * 14), []);
+  const [display, setDisplay] = useState(true);
   return (
-    <div style={{
-      width: "100%",
-      height: "100vh",
-      backgroundSize:"cover",
-      position: "relative",
-      backgroundPosition: "center"}} className={`img${randomBg}`}>
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        backgroundSize: "cover",
+        position: "relative",
+        backgroundPosition: "center",
+      }}
+      className={`img${randomBg}`}
+    >
       <StyledContainer display={String(display)}>
-        <AppRoute  logs= {logs}></AppRoute>
+        <AppRoute logs={logs}></AppRoute>
       </StyledContainer>
-      <StyledBtn onClick={()=>{setDisplay((prev)=>!prev)}}>{display? "배경 관람":"입력 돌아가기"}</StyledBtn>
+      <StyledBtn
+        onClick={() => {
+          setDisplay((prev) => !prev);
+        }}
+      >
+        {display ? "배경 관람" : "입력 돌아가기"}
+      </StyledBtn>
     </div>
   );
 }
